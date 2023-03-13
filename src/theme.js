f72ce3da3fdf68eb5f989c12c5bf90c66862a99d
Simas <59896843+simasjonelis@users.noreply.github.com>
2023-03-13 21:31:37 +0200

    new file:   src/index.js new file:   src/theme.js

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#00897b',
+    },
+    secondary: {
+      main: '#f48fb1',
+    },
+  },
+});
+
+export default theme;
