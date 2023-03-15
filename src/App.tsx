d1609222e34179be6c97f7a4f28b7032bf7a91f6
SarunasJ-gif <sarunasj82@gmail.com>
2023-03-15 10:22:50 +0200

    adding router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route } from 'react-router';
+import { Routes } from 'react-router-dom';
 import './App.css';
+import { ROUTES } from './routes/routes';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sprint</h1>
+      <Routes>
+        <Route path={ROUTES.HOME} element={''} />
+        <Route path={ROUTES.MY_CALENDER} element={''} />
+        <Route path={ROUTES.MANAGER_TEAM} element={''} />
+        <Route path={ROUTES.NEW_SPRINT} element={''} />
+      </Routes>
     </div>
   );
 }
---
+++ src/routes/routes.ts
@@ -0,0 +1,8 @@
+export enum ROUTES {
+    HOME = "/",
+    MY_CALENDER = "/calendar",
+    MANAGER_TEAM = "/manager-team",
+    SPRINT_REPORT = "/sprint-report",
+    NEW_SPRINT = "/new-sprint"
+
+}
