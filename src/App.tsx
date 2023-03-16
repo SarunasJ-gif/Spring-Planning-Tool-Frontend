626eab357ecee4bac5ddea7a49119958cc0d0332
Raigardas <raigardas.tautkus@knf.stud.vu.lt>
2023-03-16 18:11:14 +0200

    fixed BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import NavigationBar from './components/NavigationBar/NavigationBar';
 
 import { Route } from 'react-router';
 import { Routes } from 'react-router-dom';
-import { Switch } from 'react-router-dom';
 import './App.css';
 import { ROUTES } from './routes/routes';
 import theme from './theme';
@@ -15,15 +14,13 @@ function App() {
     <>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Switch>
-          <NavigationBar />
-          <Routes>
-            <Route path={ROUTES.HOME} element={''} />
-            <Route path={ROUTES.MY_CALENDER} element={''} />
-            <Route path={ROUTES.MANAGER_TEAM} element={''} />
-            <Route path={ROUTES.NEW_SPRINT} element={''} />
-          </Routes>
-        </Switch>
+        <NavigationBar />
+        <Routes>
+          <Route path={ROUTES.HOME} element={''} />
+          <Route path={ROUTES.MY_CALENDER} element={''} />
+          <Route path={ROUTES.MANAGER_TEAM} element={''} />
+          <Route path={ROUTES.NEW_SPRINT} element={''} />
+        </Routes>
       </ThemeProvider>
     </>
   );
