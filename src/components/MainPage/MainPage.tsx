e78332a7ce9d3b98a4178765c944a099aa4a8e97
Simas <59896843+simasjonelis@users.noreply.github.com>
2023-03-22 17:51:50 +0200

    sourcery-11-fix-danyla-problem

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -2,7 +2,12 @@ import * as React from 'react';
 import { Box } from '@mui/material/';
 
 function MainPage() {
-  return <Box>MAIN</Box>;
+  return (
+    <Box>
+      <h1>SOURCERY-17</h1>
+      <p>01.06.2023-04.07.2023</p>
+    </Box>
+  );
 }
 
 export default MainPage;
