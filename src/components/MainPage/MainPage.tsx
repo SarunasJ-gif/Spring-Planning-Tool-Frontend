f522232fb7a0c9c9f3f1de8b1f312f53bf4e150a
Simas <59896843+simasjonelis@users.noreply.github.com>
2023-03-23 08:35:13 +0200

    Add code to the MainPage

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,11 +1,33 @@
 import * as React from 'react';
-import { Box } from '@mui/material/';
+import { Box, Typography } from '@mui/material/';
 
 function MainPage() {
   return (
-    <Box>
-      <h1>SOURCERY-17</h1>
-      <p>01.06.2023-04.07.2023</p>
+    <Box sx={{ marginLeft: '30mm', mt: 4 }}>
+      <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
+        &ldquo;Soucery Students&rdquo; - Sprint 17
+      </Typography>
+      <Box sx={{ fontSize: '2px' }}>
+        <Typography variant="body2" sx={{ fontSize: '10px' }}>
+          29/09/2021 - 10/12/2021
+        </Typography>
+      </Box>
+
+      <Box sx={{ mt: 2 }}>
+        <Typography variant="body1" sx={{ fontWeight: 'bold' }}>
+          Tasks
+        </Typography>
+      </Box>
+      <Box sx={{ mt: 2 }}>
+        <Typography variant="body1" sx={{ fontWeight: 'bold' }}>
+          Current Plan
+        </Typography>
+      </Box>
+      <Box sx={{ mt: 2 }}>
+        <Typography variant="body1" sx={{ fontWeight: 'bold' }}>
+          Initial Plan
+        </Typography>
+      </Box>
     </Box>
   );
 }
