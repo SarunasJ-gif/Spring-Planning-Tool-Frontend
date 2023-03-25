fd0917915dd5768d7b6bcd96c35b302730156abb
Danylo Abramov <danyaa2908@gmail.com>
2023-03-25 15:02:02 +0200

    ThemeProvider was added + dynamic Topmerge

--- src/components/ErrorPage/NotFound.tsx
+++ src/components/ErrorPage/NotFound.tsx
@@ -1,31 +1,10 @@
 import React from 'react';
 import { Typography, Box, Button } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import image from './404error.png';
 import { Endpoint } from '../../routes/Endpoint';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 
-const theme = createTheme({
-  typography: {
-    fontFamily: 'Open Sans',
-    fontWeightBold: 600,
-    h2: {
-      fontSize: '36px',
-      lineHeight: '44px',
-      color: '#979797',
-      textAlign: 'center',
-    },
-    h3: {
-      fontSize: '24px',
-      lineHeight: '112px',
-      color: '#979797',
-      letterSpacing: '-1.5px',
-      textAlign: 'center',
-    },
-  },
-});
-
-const NotFound = ({ marginTop }: { marginTop?: string }) => {
+const NotFound = () => {
   const navigate = useNavigate();
 
   const handleClick = () => {
@@ -33,57 +12,87 @@ const NotFound = ({ marginTop }: { marginTop?: string }) => {
   };
 
   return (
-    <ThemeProvider theme={theme}>
-      <Box
+    <Box
+      sx={{
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        marginTop: '146px',
+      }}
+    >
+      <Typography
+        variant="h2"
         sx={{
-          width: '100%',
+          fontFamily: 'Open Sans',
+          fontStyle: 'normal',
+          fontWeight: 600,
+          fontSize: '36px',
+          lineHeight: '44px',
           display: 'flex',
-          flexDirection: 'column',
           alignItems: 'center',
-          marginTop: marginTop || '146px',
+          textAlign: 'center',
+          color: '#979797',
         }}
       >
-        <Typography variant="h2">Oops!</Typography>
+        Oops!
+      </Typography>
 
-        <Box
-          sx={{
-            width: '432px',
-            height: '234px',
-            marginTop: '69px',
-          }}
-        >
-          <img src={image} alt="image for error" />
-        </Box>
-
-        <Typography variant="h3">Error: 404 Page Not Found</Typography>
-
-        <Button
-          onClick={handleClick}
-          sx={{
-            varient: 'contained',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            padding: 0,
-            width: '195px',
-            height: '42px',
-            background: '#404CFA',
-            boxShadow:
-              '0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px rgba(0, 0, 0, 0.14), 0px 1px 5px rgba(0, 0, 0, 0.12)',
-            borderRadius: '4px',
-            color: '#FFFFFF',
-            transition: 'all 0.3s ease-in-out',
-            '&:hover': {
-              background: '#404CFA',
-              color: '#FFFFFF',
-            },
-          }}
-        >
-          BACK TO MAIN PAGE
-        </Button>
+      <Box
+        sx={{
+          width: '432px',
+          height: '234px',
+          marginTop: '69px',
+        }}
+      >
+        <img src={image} alt="image for error" />
       </Box>
-    </ThemeProvider>
+
+      <Typography
+        variant="h3"
+        sx={{
+          fontFamily: 'Open Sans',
+          fontStyle: 'normal',
+          fontWeight: 600,
+          fontSize: '24px',
+          lineHeight: '112px',
+          display: 'flex',
+          alignItems: 'center',
+          textAlign: 'center',
+          letterSpacing: '-1.5px',
+          color: '#979797',
+          marginTop: '15px',
+        }}
+      >
+        Error: 404 Page Not Found
+      </Typography>
+
+      <Button
+        onClick={handleClick}
+        sx={{
+          varient: 'contained',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          padding: 0,
+          width: '195px',
+          height: '42px',
+          background: '#404CFA',
+          boxShadow:
+            '0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px rgba(0, 0, 0, 0.14), 0px 1px 5px rgba(0, 0, 0, 0.12)',
+          borderRadius: '4px',
+          color: '#FFFFFF',
+          transition: 'all 0.3s ease-in-out',
+          '&:hover': {
+            background: '#404CFA',
+            color: '#FFFFFF',
+          },
+        }}
+      >
+        BACK TO MAIN PAGE
+      </Button>
+    </Box>
   );
 };
 
