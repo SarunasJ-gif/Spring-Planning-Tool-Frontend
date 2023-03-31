a9dadc119fa0b596ea0b95a865aee346ab88acc0
Simas <59896843+simasjonelis@users.noreply.github.com>
2023-03-31 12:00:36 +0300

    Fix some mistakes

--- src/components/MainPage/MainPage.tsx
+++ src/components/MainPage/MainPage.tsx
@@ -1,49 +1,46 @@
 import * as React from 'react';
-import { Box, Typography, Accordion, AccordionSummary } from '@mui/material/';
+import { Box, Typography, AccordionSummary } from '@mui/material/';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { SAccordion } from '../../style/AccordionStyle';
 
 function MainPage() {
   return (
-    <Box sx={{ marginLeft: '30mm', mt: 4 }}>
-      <Typography variant="h5" sx={{ fontWeight: 'bold' }}>
-        &ldquo;Soucery Students&rdquo; - Sprint 17
+    <Box sx={{ marginLeft: '30mm', marginRight: '10mm', mt: 4 }}>
+      <Typography variant="h4" sx={{ fontWeight: 'bold' }}>
+        &ldquo;Sourcery Students&rdquo; - Sprint 17
       </Typography>
-      <Box sx={{ fontSize: '10px' }}>
-        <Typography variant="body2">29/09/2021 - 10/12/2021</Typography>
+      <Box>
+        <Typography sx={{ fontFamily: 'Poppins' }}>
+          29/09/2021 - 10/12/2021
+        </Typography>
       </Box>
 
       <Box sx={{ mt: 4 }}>
-        <Accordion>
+        <SAccordion sx={{ display: 'flex', justifyContent: 'left' }}>
           <AccordionSummary
             expandIcon={<ExpandMoreIcon style={{ fill: '#404CFA' }} />}
           >
-            <Typography variant="body1" sx={{ fontWeight: 'bold' }}>
-              Tasks
-            </Typography>
+            <Typography>Tasks</Typography>
           </AccordionSummary>
-        </Accordion>
+        </SAccordion>
       </Box>
       <Box sx={{ mt: 2 }}>
-        <Accordion>
+        <SAccordion sx={{ display: 'flex', justifyContent: 'left' }}>
           <AccordionSummary
             expandIcon={<ExpandMoreIcon style={{ fill: '#404CFA' }} />}
           >
-            <Typography variant="body1" sx={{ fontWeight: 'bold' }}>
-              Current Plan
-            </Typography>
+            <Typography>Current Plan</Typography>
           </AccordionSummary>
-        </Accordion>
+        </SAccordion>
       </Box>
       <Box sx={{ mt: 2 }}>
-        <Accordion>
+        <SAccordion sx={{ display: 'flex', justifyContent: 'left' }}>
           <AccordionSummary
             expandIcon={<ExpandMoreIcon style={{ fill: '#404CFA' }} />}
           >
-            <Typography variant="body1" sx={{ fontWeight: 'bold' }}>
-              Initial Plan Details
-            </Typography>
+            <Typography>Initial Plan</Typography>
           </AccordionSummary>
-        </Accordion>
+        </SAccordion>
       </Box>
     </Box>
   );
--- src/theme.ts
+++ src/theme.ts
@@ -1,12 +1,24 @@
 import { createTheme } from '@mui/material/styles';
+import 'typeface-poppins';
 
 const theme = createTheme({
   palette: {
+    background: {
+      default: '#E5E5E5',
+    },
     primary: {
       main: '#404CFA',
     },
     secondary: {
       main: '#E5E5E5',
+    },
+  },
+  typography: {
+    fontFamily: 'Poppins',
+    h4: {
+      fontWeight: '100',
+      fontSize: '20px',
+      border: 'none',
     },
   },
 });
