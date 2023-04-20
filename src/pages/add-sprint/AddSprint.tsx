2666cbcda06eb041da172592f40008ac07ce0bbe
Tadasls <tadas.laurinaitis@gmail.com>
2023-04-20 12:31:48 +0300

    added Bottom Meniu to add sprint page with Add and Cancel Buttons

--- src/pages/add-sprint/AddSprint.tsx
+++ src/pages/add-sprint/AddSprint.tsx
@@ -1,8 +1,20 @@
 import NewTask from "../../components/NewTask/NewTask";
+import BottomMenu from "../../components/BottomMenu/BottomMenu";
+import { Box } from "@mui/material";
 
 function AddSprint() {
   return (
-          <NewTask />
+    <Box 
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "space-between",
+        height: "92vh",
+      }}
+    >
+      <NewTask />
+      <BottomMenu />
+    </Box>   
   );
 }
 export default AddSprint;
