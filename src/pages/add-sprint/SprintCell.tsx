82bae2e42f2cdb4c723544ca4558655340aa4d6f
Danylo_Abramov <danyaa2908@gmail.com>
2023-04-21 13:19:50 +0300

    Changed Cell and fixed a problem

--- src/pages/add-sprint/SprintCell.tsx
+++ src/pages/add-sprint/SprintCell.tsx
@@ -1,109 +1,32 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Box, ThemeProvider, Typography } from '@mui/material';
-import './SprintCell.css';
+import React, { useRef } from 'react';
+import { TextField, ThemeProvider } from '@mui/material';
 import theme from './theme';
 
 interface SprintCellProps {
   name: string;
-  setName: (name: string) => void;
 }
 
-export const SprintCell = ({ name, setName }: SprintCellProps) => {
-  const [editing, setEditing] = useState(name.length === 0);
-  const [tempName, setTempName] = useState(name);
-  const [isInvalid, setIsInvalid] = useState(tempName.length === 0);
-
-  const handleDoubleClick = () => {
-    setEditing(true);
-    setTempName(name || '');
-  };
+export const SprintCell = ({ name}: SprintCellProps) => {
   const inputRef = useRef<HTMLInputElement>(null);
 
-  useEffect(() => {
-    if (editing && inputRef.current) {
-      inputRef.current.focus();
-      inputRef.current.setSelectionRange(tempName.length, tempName.length);
-    }
-  }, [editing]);
-
-  const handleBlur = () => {
-    if (tempName.trim().length === 0) {
-      setEditing(true);
-      setTempName('');
-      setIsInvalid(true);
-    } else {
-      setEditing(false);
-      setIsInvalid(false);
-      setName(tempName.trim());
-    }
-  };
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const value = event.target.value;
-    setTempName(value);
-    setIsInvalid(value.trim().length === 0);
-  };
-  const handleInputKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDivElement>) => {
     if (event.key === 'Enter') {
-      handleBlur();
+      event.preventDefault();
+      inputRef.current?.blur();
     }
   };
 
   return (
     <ThemeProvider theme={theme}>
-    <Box
-      className="SprintCell"
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'left',
-        width: '350px',
-        padding: '12px',
-        marginTop: '15px',
-        backgroundColor: '#D8DAFF',
-        border: '1px solid #E1E5EB',
-        borderRadius: '4px 4px 0px 0px',
-        boxSizing: 'border-box',
-        marginLeft: '110px',
-        borderBottom: '2px solid rgba(0, 0, 0, 0.6)',
-      }}
-    >
-      <Typography variant="caption">
-        Sprint Name
-      </Typography>
-      <div onDoubleClick={handleDoubleClick}>
-        {editing ? (
-          <input
-            ref={inputRef}
-            type="text"
-            value={tempName}
-            onChange={handleInputChange}
-            onBlur={handleBlur}
-            onKeyDown={handleInputKeyDown}
-            className={`${isInvalid ? 'invalid' : ''}`}
-            style={{ width: '100%' }}
-          />
-        ) : (
-          <Typography
-            variant="body1"
-            sx={{
-              fontFamily: 'Roboto',
-              fontStyle: 'normal',
-              fontWeight: 400,
-              fontSize: '16px',
-              lineHeight: '24px',
-              letterSpacing: '0.15px',
-              color: 'rgba(0, 0, 0, 0.87)',
-              marginTop: 'auto',
-              alignSelf: 'flex-start',
-              cursor: 'text',
-              marginBottom: '3px',
-            }}
-          >
-            {name}
-          </Typography>
-        )}
-      </div>
-    </Box>
+      <TextField
+        id="filled-basic"
+        label="Sprint Name"
+        variant="filled"
+        defaultValue={name}
+        style={{ width: '328px', marginLeft: '88px' }}
+        inputRef={inputRef}
+        onKeyDown={handleKeyDown}
+      />
     </ThemeProvider>
   );
 };
