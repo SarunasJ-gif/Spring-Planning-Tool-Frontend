43d800f457678901de84b1af16e1a8f8fda8d703
Raigardas <raigardas.tautkus@knf.stud.vu.lt>
2023-05-18 16:30:03 +0300

    implemented route validation

---
+++ src/routes/ProtectedRoute.tsx
@@ -0,0 +1,42 @@
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+type ProtectedRouteProps = {
+  element: React.ReactNode;
+  path: string;
+};
+
+const validateToken = (token: string): boolean => {
+  try {
+    const payload = token.split('.')[1];
+    const decodedPayload = window.atob(payload);
+    const parsedClaims = JSON.parse(decodedPayload);
+
+    const currentTime = Date.now() / 1000;
+    if (parsedClaims.exp && parsedClaims.exp < currentTime) {
+      console.error('JWT token expired');
+      return false;
+    }
+
+    return true;
+  } catch (error) {
+    console.error('Invalid JWT token:', error);
+    return false;
+  }
+};
+
+const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ element, path }) => {
+  const navigate = useNavigate();
+  const token = localStorage.getItem('token');
+  const isAuthenticated = token ? validateToken(token) : false;
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate('/login');
+    }
+  }, [isAuthenticated, navigate]);
+
+  return isAuthenticated ? <>{element}</> : null;
+};
+
+export default ProtectedRoute;
