cfd69de9c3a152d83eedb897804c843bd3218bd3
Tadasls <tadas.laurinaitis@gmail.com>
2023-05-19 15:37:20 +0300

    back to work

--- src/redux/ManageTeam/ManageTeamApi.ts
+++ src/redux/ManageTeam/ManageTeamApi.ts
@@ -4,13 +4,13 @@ import { Member } from "../../types/NewSprintTypes";
 import { Team } from "../../types/TeamTypes";
 
 export const getTeamDataAPI = () => { return get<Team>("/team");} 
-export const getTeamDataApiMAIN = () => { return get<Team>("/team/0");} 
+export const getTeamDataApiMAIN = () => { return get<Team>("/team/1");} 
 export const updateTeamNameAPI = (teamId: number, name: String) => { return put(`/${teamId}/update/`,{ name }); };
 
-export const getTeamMembersAPI = () => { return get<Member[]>("/member/team/0");} 
-export const addTeamMemberAPI = ( id: number) => { post(`/team/0/member`, {id});} 
+export const getTeamMembersAPI = () => { return get<Member[]>("/member/team/1");} 
+export const addTeamMemberAPI = ( id: number) => { post(`/team/1/member`, {id});} 
 
-export const removeTeamMember = (memberId: number) => { return remove(`/team/0/member/${memberId}`)}; 
+export const removeTeamMember = (memberId: number) => { return remove(`/team/1/member/${memberId}`)}; 
 
 export const getMembersAPI = () => { return get<Member[]>("/member") ;} 
 export const updateTeamMemberRole = (id: number, role: Role) => { return put(`/member/role`, {id, role }); };
